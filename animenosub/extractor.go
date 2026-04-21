04afd35ece908aba09ce86d6313379bdcaf5fcd0
dhilzyi <revaldiefahrezal@gmail.com>
2026-04-22 00:18:14 +0700

    feat: start adding animenosub module

---
+++ animenosub/extractor.go
@@ -0,0 +1,131 @@
+package animenosub
+
+import (
+	"encoding/base64"
+	"fmt"
+	"net/http"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+type seriesData struct {
+	url           string
+	episodeNumber int
+	episodeTitle  string
+}
+
+type server struct {
+	Type       string
+	ServerName string
+	Value      string
+}
+
+var baseUrl string = "https://animenosub.to"
+
+func GetSeriesData(url string) ([]seriesData, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	epsLister := doc.Find(".eplister")
+
+	var seriesEpisode []seriesData
+	epsLister.Find("a").Each(func(i int, s *goquery.Selection) {
+		href, exist := s.Attr("href")
+		if !exist {
+			fmt.Println("Info: Could not find href in 'a' element")
+			return
+		}
+		epsNumRaw := s.Find(".epl-num")
+		epsNumInt, err := strconv.Atoi(epsNumRaw.Text())
+		if err != nil {
+			fmt.Println("Error: Fail to epsNumRaw convert to int")
+			return
+		}
+		epsTitle := s.Find(".epl-title")
+
+		instance := seriesData{
+			url:           href,
+			episodeNumber: epsNumInt,
+			episodeTitle:  strings.TrimSpace(epsTitle.Text()),
+		}
+		seriesEpisode = append(seriesEpisode, instance)
+	})
+
+	return seriesEpisode, nil
+}
+
+func GetServerLink(url string) ([]server, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	itemVideoNav := doc.Find(".item.video-nav")
+
+	var serverList []server
+	itemVideoNav.Find("option").Each(func(i int, s *goquery.Selection) {
+		value, exist := s.Attr("value")
+		if !exist {
+			fmt.Println("Info: Could not find value in 'option' element")
+			return
+		}
+		if value == "" {
+			return
+		}
+
+		var typeServer string
+		if strings.Contains(s.Text(), "SUB") {
+			typeServer = "SUB"
+		} else {
+			typeServer = "RAW"
+		}
+		instance := server{
+			Type:       typeServer,
+			Value:      value,
+			ServerName: strings.TrimSpace(s.Text()),
+		}
+		serverList = append(serverList, instance)
+	})
+
+	return serverList, nil
+}
+
+func decodeBase64(input string) (string, error) {
+	decodeByte, err := base64.StdEncoding.DecodeString(input)
+	if err != nil {
+		return "", err
+	}
+
+	return string(decodeByte), nil
+}
+
+func decodeB64URL(input string) ([]byte, error) {
+	return base64.RawURLEncoding.DecodeString(input)
+}
+
+func getIframeSrc(rawhtml string) (string, error) {
+	re := regexp.MustCompile(`src\s*=\s*"([^"]+)"`)
+	matches := re.FindStringSubmatch(rawhtml)
+	if len(matches) > 1 {
+		src := matches[1]
+		return src, nil
+	}
+
+	return "", fmt.Errorf("Error: No src found")
+}
---
+++ animenosub/moon_extractor.go
@@ -0,0 +1,257 @@
+package animenosub
+
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"path"
+	"strings"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type detailsResponse struct {
+	EmbedFrameUrl string `json:"embed_frame_url"`
+}
+
+type fingerprint struct {
+	ViewerId   string  `json:"viewer_id"`
+	DeviceId   string  `json:"device_id"`
+	Confidence float32 `json:"confidence"`
+	Now        int64   `json:"iat"`
+	Exp        int64   `json:"exp"`
+}
+
+type fingerprintRequest struct {
+	Fingerprint fingerprintData `json:"fingerprint"`
+}
+
+type fingerprintData struct {
+	Token      string  `json:"token"`
+	ViewerId   string  `json:"viewer_id"`
+	DeviceId   string  `json:"device_id"`
+	Confidence float64 `json:"confidence"`
+}
+
+type PlaybackResponse struct {
+	Playback struct {
+		Algorithm   string    `json:"algorithm"`
+		Iv          string    `json:"iv"`
+		Payload     string    `json:"payload"`
+		KeyParts    []string  `json:"key_parts"`
+		ExpiresAt   time.Time `json:"expires_at"`
+		DecryptKeys struct {
+			Edge1          string `json:"edge_1"`
+			Edge2          string `json:"edge_2"`
+			LegacyFallback string `json:"legacy_fallback"`
+		} `json:"decrypt_keys"`
+		Iv2      string `json:"iv2"`
+		Payload2 string `json:"payload2"`
+	} `json:"playback"`
+}
+
+type SourceData struct {
+	Sources []struct {
+		Quality     string `json:"quality"`
+		Label       string `json:"label"`
+		MimeType    string `json:"mime_type"`
+		URL         string `json:"url"`
+		BitrateKbps int    `json:"bitrate_kbps"`
+		Height      int    `json:"height"`
+		SizeBytes   int    `json:"size_bytes"`
+	} `json:"sources"`
+	Tracks    []interface{} `json:"tracks"`
+	PosterURL string        `json:"poster_url"`
+}
+
+type StreamData struct {
+	Url    string
+	Header http.Header
+}
+
+func videosFromUrl(videoUrl string) (string, error) {
+	parsedUrl, err := url.Parse(videoUrl)
+	if err != nil {
+		return "", err
+	}
+	videoId := path.Base(parsedUrl.Path)
+	detailsUrl := fmt.Sprintf("https://%s/api/videos/%s/embed/details", parsedUrl.Host, videoId)
+	req, err := http.NewRequest("GET", detailsUrl, nil)
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("Referer", videoUrl+"/")
+	req.Header.Set("Origin", videoUrl)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	var detailsData detailsResponse
+	if err := json.NewDecoder(resp.Body).Decode(&detailsData); err != nil {
+		return "", err
+	}
+
+	return detailsData.EmbedFrameUrl, nil
+}
+
+func getEmbedData(embedUrl, siteUrl string) error {
+	parsedUrl, err := url.Parse(embedUrl)
+	if err != nil {
+		return err
+	}
+	videoId := path.Base(parsedUrl.Path)
+
+	fingerprintBody, err := buildFingerprintBody()
+	if err != nil {
+		return err
+	}
+
+	bodyBytes, err := json.Marshal(fingerprintBody)
+	if err != nil {
+		return err
+	}
+
+	playbackUrl := fmt.Sprintf("https://%s/api/videos/%s/embed/playback", parsedUrl.Host, videoId)
+	playbackReq, err := http.NewRequest("POST", playbackUrl, bytes.NewReader(bodyBytes))
+	if err != nil {
+		return err
+	}
+
+	originHost := strings.TrimPrefix(siteUrl, "https://")
+	originHost = strings.TrimPrefix(originHost, "http://")
+	originHost = strings.TrimSuffix(originHost, "/")
+	userAgent := "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
+
+	playbackReq.Header.Set("X-Embed-Origin", originHost)
+
+	playbackReq.Header.Set("Referer", embedUrl)
+	playbackReq.Header.Set("Origin", "https://"+parsedUrl.Host)
+	playbackReq.Header.Set("User-Agent", userAgent)
+
+	playbackReq.Header.Set("X-Embed-Origin", originHost)
+	playbackReq.Header.Set("X-Embed-Parent", fmt.Sprintf("https://%s/e/%s", parsedUrl.Host, videoId))
+	playbackReq.Header.Set("X-Embed-Referer", siteUrl+"/")
+	playbackReq.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	playbackResp, err := client.Do(playbackReq)
+	if err != nil {
+		return err
+	}
+	defer playbackResp.Body.Close()
+
+	var responseData PlaybackResponse
+	if err := json.NewDecoder(playbackResp.Body).Decode(&responseData); err != nil {
+		return err
+	}
+	streamByte, err := decryptPayload(responseData)
+	if err != nil {
+		return err
+	}
+	var source SourceData
+	if err := json.Unmarshal(streamByte, &source); err != nil {
+		return err
+	}
+	if source.Sources[0].URL == "" {
+		return fmt.Errorf("Error: No masterUrl found")
+	}
+	masterUrl := source.Sources[0].URL
+	header := http.Header{}
+	header.Set("Referer", fmt.Sprintf("https://%s/", parsedUrl.Host))
+	header.Set("Origin", fmt.Sprintf("https://%s", parsedUrl.Host))
+	header.Set("User-Agent", userAgent)
+
+	streamData := StreamData{
+		Url:    masterUrl,
+		Header: header,
+	}
+
+	return nil
+}
+
+func decryptPayload(pb PlaybackResponse) ([]byte, error) {
+	keyPart1, err := base64.RawURLEncoding.DecodeString(pb.Playback.KeyParts[0])
+	if err != nil {
+		return nil, err
+	}
+
+	keyPart2, err := base64.RawURLEncoding.DecodeString(pb.Playback.KeyParts[1])
+	if err != nil {
+		return nil, err
+	}
+
+	keyBytes := append(keyPart1, keyPart2...)
+
+	ivBytes, err := base64.RawURLEncoding.DecodeString(pb.Playback.Iv)
+	if err != nil {
+		return nil, err
+	}
+
+	cipherBytes, err := base64.RawURLEncoding.DecodeString(pb.Playback.Payload)
+	if err != nil {
+		return nil, err
+	}
+
+	block, err := aes.NewCipher(keyBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	sourceByte, err := gcm.Open(nil, ivBytes, cipherBytes, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return sourceByte, nil
+}
+
+func buildFingerprintBody() (fingerprintRequest, error) {
+	viewerUUID, _ := uuid.NewRandom()
+	deviceUUID, _ := uuid.NewRandom()
+
+	viewerId := strings.ReplaceAll(viewerUUID.String(), "-", "")
+	deviceId := strings.ReplaceAll(deviceUUID.String(), "-", "")
+
+	nowSec := time.Now().Unix()
+	expSec := nowSec + 600
+
+	fingerprintPayload := fingerprint{
+		ViewerId:   viewerId,
+		DeviceId:   deviceId,
+		Confidence: 0.93,
+		Now:        nowSec,
+		Exp:        expSec,
+	}
+
+	rawByte, err := json.Marshal(fingerprintPayload)
+	if err != nil {
+		return fingerprintRequest{}, err
+	}
+	encodePayload := base64.RawURLEncoding.EncodeToString(rawByte)
+
+	fingerprintToken := encodePayload + ".AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+	fingerprintBody := fingerprintRequest{
+		Fingerprint: fingerprintData{
+			Token:      fingerprintToken,
+			ViewerId:   viewerId,
+			DeviceId:   deviceId,
+			Confidence: 0.93,
+		},
+	}
+	return fingerprintBody, nil
+}
