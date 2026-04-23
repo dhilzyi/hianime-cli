3b46b6b77035ebad5c8d961e9a571e502a7de91a
dhilzyi <revaldiefahrezal@gmail.com>
2026-04-23 11:14:11 +0700

    feat: add new anilist module and move jimaku to internal module
    - anilist for retrieving anilistID use for querying jimaku subtitle
    - also update jimaku code to call anilist query if anilistID not exist

---
+++ internal/anilist/anilist.go
@@ -0,0 +1,37 @@
+package anilist
+
+import (
+	"fmt"
+
+	"github.com/dhilzyi/hianime-cli/internal/core"
+)
+
+func FillSeriesData(seriesData *core.SeriesData) error {
+	var title string
+	if seriesData.Titles.EnglishTitle != "" {
+		title = seriesData.Titles.EnglishTitle
+	} else if seriesData.Titles.RomajiTitle != "" {
+		title = seriesData.Titles.RomajiTitle
+	} else if seriesData.Titles.KanjiTitle != "" {
+		title = seriesData.Titles.KanjiTitle
+	} else {
+		return fmt.Errorf("Error: All titles are empty and query can not be process")
+	}
+
+	graphresp, err := getAnilistData(title)
+	if err != nil {
+		return err
+	}
+	seriesData.AnilistID = graphresp.Data.Media.ID
+	if seriesData.Titles.EnglishTitle == "" {
+		seriesData.Titles.EnglishTitle = graphresp.Data.Media.Title.English
+	}
+	if seriesData.Titles.RomajiTitle == "" {
+		seriesData.Titles.RomajiTitle = graphresp.Data.Media.Title.Romaji
+	}
+	if seriesData.Titles.KanjiTitle == "" {
+		seriesData.Titles.KanjiTitle = graphresp.Data.Media.Title.Native
+	}
+
+	return nil
+}
